fe8c3b6365bd483ab7eaf171822577b3b9719bd3
Stefanos <stefanosu422@gmail.com>
2018-10-03 21:00:41 -0400

    Updated test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 
 expect(add(5, 10)).toBe(15); 
 
-it('should consider this spec'.fucntion(){
+it('should consider this spec'.function(){
 	expect(true).toBe(true);
 	expect(false).not.toBe(false);
 
